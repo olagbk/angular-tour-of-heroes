5a1e47f69fda2d44e32e1b338d83b11fb79c2a37
Alexandra Gabka <rivvel@gmail.com>
2017-08-22 14:20:22 +0200

    get heroes list from a service (s5)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,8 @@
 import { Component } from '@angular/core';
 
 import { Hero } from './hero'
+import { HeroService } from './hero.service';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 @Component({
   selector: 'my-app',
@@ -29,9 +18,8 @@ const HEROES: Hero[] = [
       </li>
     </ul>
     <hero-detail [hero]="selectedHero"></hero-detail>
-
-
   `,
+  providers: [HeroService],
   styles: [`
     .selected {
       background-color: #CFD8DC !important;
@@ -84,9 +72,19 @@ const HEROES: Hero[] = [
 })
 export class AppComponent  {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService){}
+
+  ngOnInit(): void{
+    this.getHeroes();
+  }
+
   onSelect(hero:Hero): void {
     this.selectedHero = hero;
+  }
+  getHeroes(): void{
+    this.heroService.getHeroes().then( heroes => this.heroes = heroes);
   }
 }
