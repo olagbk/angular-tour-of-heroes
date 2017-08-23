4d860e805adf035048ffa8877c0a85f1a79c532d
Alexandra Gabka <rivvel@gmail.com>
2017-08-23 12:56:51 +0200

    add routing and add/edit/delete functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import {DashboardComponent} from "./dashboard.component";
 
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   imports:      [ BrowserModule,
-                  FormsModule
+                  FormsModule,
+                  HttpModule,
+                  AppRoutingModule,
+                  InMemoryWebApiModule.forRoot(InMemoryDataService)
                 ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, DashboardComponent ],
+  providers:    [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
